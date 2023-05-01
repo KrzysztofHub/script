d6b225da8fbaef9aa7c271015cfaf35990ecc312
KrzysztofHub <117397313+KrzysztofHub@users.noreply.github.com>
2023-05-01 21:14:34 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 12639799777 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/noob69420000/YoMao/main/gui.lua"))()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/KrzysztofHub/YoMao/main/gui.lua"))()
 end
