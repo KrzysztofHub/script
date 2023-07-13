b0e19c60b45a307c1666c00256e3fc799d34208c
KrzysztofHub <117397313+KrzysztofHub@users.noreply.github.com>
2023-07-13 20:14:18 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,12 @@
 if game.PlaceId == 12639799777 then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/KrzysztofHub/YoMao/main/gui.lua"))()
 	end
-if game.PlaceId == 12404957109 then
+--[[if game.PlaceId == 12404957109 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/KrzysztofHub/Games/main/AnimeStreetSimulator"))()
 	end
+
+not going to finish it :angry:
+]]--
 if game.PlaceId == 13127800756 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/KrzysztofHub/Games/main/ArmWrestleSimulator.lua"))()
 end
